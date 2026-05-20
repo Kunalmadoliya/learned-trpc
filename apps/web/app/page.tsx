2cd32a6e5705e3389dccd1319fce8983374891aa
Kunal Madoliya <madoliyakunal@gmail.com>
2026-05-20 17:50:32 +0530

    feat: initialize landing page component in web app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,9 @@
 "use client";
-import { trpc } from "~/trpc/client";
 
 export default function Home() {
-  const { data } = trpc.chaicode.useQuery({ email: "Kunalmadoliya@gmail.com", name: "Kunal", phone: 1234567890 });
   return (
     <main className="min-h-screen min-w-screen flex justify-center items-center">
-      <div>
-        <p>{data?.message}</p>
-      </div>
+      <div></div>
     </main>
   );
 }
